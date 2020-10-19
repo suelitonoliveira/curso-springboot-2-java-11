52d55fb00c0540b708b36a27860dbe1b6a0887a4
suelitonoliveira <suelitondeoliveira@gmail.com>
2020-10-19 09:15:33 -0300

    OrderRepository

---
+++ src/main/java/com/educandoweb/curso/repositories/OrderRepository.java
@@ -0,0 +1,10 @@
+package com.educandoweb.curso.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.educandoweb.curso.entities.Order;
+
+
+public interface OrderRepository extends JpaRepository<Order, Long>{
+
+}
